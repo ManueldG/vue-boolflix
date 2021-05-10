9dc6faba6ae016f70b6bc11dcdad2f335973f855
Manuel della Gala <manuel.dellagala@gmail.com>
2021-05-10 13:45:40 +0200

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--header-->
+    <Main/><!--main-->
+    <!--footer-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
---
+++ src/components/Main.vue
@@ -0,0 +1,56 @@
+<template>
+    <main id="Main" >
+        <ul>
+            <li v-for="(el,index) in g.data.results" :key="index">
+                {{el.original_title}}
+
+            </li>
+        </ul>
+        
+
+    </main>
+  
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name : 'Main',
+    data(){
+        return {
+            g:{},
+
+        }
+    },
+    methods:{
+        getData(){
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=49b7715adeda3f2ed02386b0db03af61&query=nemo')
+            .then((response) => {
+                // handle success
+                console.log("response",response);
+                this.g=response;
+                console.log("this",this);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+            
+  }
+  },
+  created(){
+      console.log("created");
+      this.getData();
+  }
+        
+    
+
+}
+</script>
+
+<style>
+
+</style>
